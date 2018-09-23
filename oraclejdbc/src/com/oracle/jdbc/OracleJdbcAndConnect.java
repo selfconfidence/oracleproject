31e8dbfc87a8ad28ab25438fd8e0d8f7005403cf
misterWei <mynameisweiyan@gmail.com>
2018-09-23 18:30:10 +0800

    two

--- oraclejdbc/src/com/oracle/jdbc/OracleJdbcAndConnect.java
+++ oraclejdbc/src/com/oracle/jdbc/OracleJdbcAndConnect.java
@@ -1,9 +1,9 @@
 package com.oracle.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import oracle.jdbc.OracleTypes;
+import org.junit.Test;
+
+import java.sql.*;
 
 /**
  * @author misterWei
@@ -13,15 +13,15 @@ import java.sql.Statement;
 public class OracleJdbcAndConnect {
     public static void main(String[] args) throws Exception {
         Class.forName("oracle.jdbc.driver.OracleDriver");
-        String url = "jdbc:oracle:thin:@192.168.238.144:1521:xe";
-        String userNanme = "system";
-        String password = "12345";
+        String url = "jdbc:oracle:thin:@192.168.238.150:1521:xe";
+        String userNanme = "manager";
+        String password = "manager";
         Connection connection = DriverManager.getConnection(url, userNanme, password);
         Statement statement = connection.createStatement();
-        ResultSet executeQuery = statement.executeQuery("select * from student");
+        ResultSet executeQuery = statement.executeQuery("select * from person");
         while (executeQuery.next()) {
-            System.out.println(executeQuery.getString("s_id"));
-            System.out.println(executeQuery.getString("s_name"));
+            System.out.println(executeQuery.getString("pid"));
+            System.out.println(executeQuery.getString("pname"));
 
         }
         connection.commit();
@@ -32,5 +32,58 @@ public class OracleJdbcAndConnect {
 
     }
 
+    @Test
+    public void executeInsert() {
+        try {
+
+            Connection connection = getConnection();
+            PreparedStatement callableStatement = connection.prepareStatement(" insert into person(pname,gender) values(?,?)");
+            callableStatement.setObject(1, "王老五");
+            callableStatement.setObject(2, 5);
+            callableStatement.execute();
+            connection.commit();
+            callableStatement.close();
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Test
+    public void function() {
+        try {
+
+            Connection connection = getConnection();
+            CallableStatement callableStatement = connection.prepareCall("{call p_yearsal(?,?)}");
+            callableStatement.setObject(1,7844);
+            callableStatement.registerOutParameter(2,OracleTypes.NUMBER);
+
+            callableStatement.execute();
+            System.out.println(callableStatement.getObject(2));
+            callableStatement.close();
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Connection getConnection() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            String url = "jdbc:oracle:thin:@192.168.238.150:1521:xe";
+            String userNanme = "manager";
+            String password = "manager";
+            Connection connection = DriverManager.getConnection(url, userNanme, password);
+            return connection;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+
+        }
+
+    }
 
 }
